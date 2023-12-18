284aeb0f8fd901459184304bcbf7db5abe387f11
Setara Ibn <133366967+Setara5@users.noreply.github.com>
2023-12-18 09:12:06 +0700

    Update update.php

--- update.php
+++ update.php
@@ -2,22 +2,19 @@
 include 'functions.php';
 $pdo = pdo_connect_mysql();
 $msg = '';
-// Check if the contact id exists, for example update.php?id=1 will get the contact with the id of 1
 if (isset($_GET['id'])) {
     if (!empty($_POST)) {
-        // This part is similar to the create.php, but instead we update a record and not insert
+	    
         $id = isset($_POST['id']) ? $_POST['id'] : NULL;
         $nama = isset($_POST['nama']) ? $_POST['nama'] : '';
         $email = isset($_POST['email']) ? $_POST['email'] : '';
         $notelp = isset($_POST['notelp']) ? $_POST['notelp'] : '';
         $pekerjaan = isset($_POST['pekerjaan']) ? $_POST['pekerjaan'] : '';
         
-        // Update the record
         $stmt = $pdo->prepare('UPDATE kontak SET id = ?, nama = ?, email = ?, notelp = ?, pekerjaan = ? WHERE id = ?');
         $stmt->execute([$id, $nama, $email, $notelp, $pekerjaan, $_GET['id']]);
         $msg = 'Updated Successfully!';
     }
-    // Get the contact from the contacts table
     $stmt = $pdo->prepare('SELECT * FROM kontak WHERE id = ?');
     $stmt->execute([$_GET['id']]);
     $contact = $stmt->fetch(PDO::FETCH_ASSOC);
