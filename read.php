e7b7a1ac2c4cc44507fcbcb14c553a9dc1ac36af
Setara Ibn <133366967+Setara5@users.noreply.github.com>
2023-12-18 09:11:34 +0700

    Update read.php

--- read.php
+++ read.php
@@ -1,14 +1,9 @@
 <?php
 include 'functions.php';
-// Connect to MySQL database
 $pdo = pdo_connect_mysql();
-// Get the page via GET request (URL param: page), if non exists default the page to 1
 $page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
-// Number of records to show on each page
 $records_per_page = 5;
 
-
-// Prepare the SQL statement and get records from our contacts table, LIMIT will determine the page
 $stmt = $pdo->prepare('SELECT * FROM kontak ORDER BY id LIMIT :current_page, :record_per_page');
 $stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
 $stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
